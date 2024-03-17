3ba4a7d7e9ac56011a7c500fe29e42e3cc458656
mavlenkulovachynara <mavlenkulovachynara@gmail.com>
2024-03-17 16:22:10 +0600

    meerim commit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 
 import MyRoutes from "./routes/MyRoutes";
 import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
 
 const App = () => {
   return (
     <>
+    <Navbar/>
       <MyRoutes />
       <Footer />
     </>
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import usericon from '../../img/default-user-icon.webp'
+import logo from '../../img/worm.png'
+import likeicon from '../../img/like (1).png'
+import bell from '../../img/bell.png'
+
+const Navbar = () => {
+  const handleNotificationClick = () => {
+    // Действие при клике на иконку уведомлений
+  };
+
+  const handleLikeClick = () => {
+    // Действие при клике на иконку лайка
+  };
+
+  const handleUserClick = () => {
+    // Действие при клике на иконку пользователя
+  };
+
+  return (
+    <div className="nav-bar">
+      <Link to="/" className="logo-link">
+        <img src={logo} alt="Логотип" className="logo-img" />
+      </Link>
+      <div className="nav-links">
+        <Link to="/" className="link">Проблемы</Link>
+        <Link to="/" className="link">Изучить</Link>
+        <Link to="/" className="link">Конкурс</Link>
+        <Link to="/" className="link">Обсуждение</Link>
+      </div>
+      <div className="nav-icons">
+        <div className="icon-wrapper" onClick={handleNotificationClick}>
+          <img src={bell} alt="Уведомления" className="icon" />
+        </div>
+        <div className="icon-wrapper" onClick={handleLikeClick}>
+          <img src={likeicon} alt="Лайк" className="icon" />
+        </div>
+        <div className="icon-wrapper" onClick={handleUserClick}>
+          <img src={usericon} alt="Пользователь" id="userIcon" className="icon" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
